102e3c04a17b9589e988b5dbc399d84e0fae4c66
DESKTOP-T0RPJT0\lovem <jermeylee4488@gmail.com>
2026-05-21 13:24:16 +0800

    修改音效撥放方式

--- Assets/scripts/AudioControl/AudioManager.cs
+++ Assets/scripts/AudioControl/AudioManager.cs
@@ -1,19 +1,49 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     [SerializeField] AudioSource[] audioSources;
+    [SerializeField] AudioClip[] audioClips;
+    
+    private Dictionary<string, AudioClip> audioLibrary;
+    
     void Awake()
     {
         if (instance == null)
         {
             instance = this;
         }
+        InitializeAudioLibrary();
     }
+    
+    private void InitializeAudioLibrary()
+    {
+        audioLibrary = new Dictionary<string, AudioClip>();
+        
+        foreach (AudioClip clip in audioClips)
+        {
+            audioLibrary[clip.name] = clip;
+        }
+    }
+    
     public void blocksuccess()
     {
         audioSources[0].Play();
+    }
+    
+
+    public void playSFX(string name)
+    {
+        if (audioLibrary.ContainsKey(name))
+        {
+            audioSources[0].PlayOneShot(audioLibrary[name]);
+        }
+        else
+        {
+            Debug.LogWarning($"找不到音效: {name}");
+        }
     }
 }
